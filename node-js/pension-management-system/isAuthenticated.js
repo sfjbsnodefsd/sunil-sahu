79ac2e5f3c36f5d30af77732217c7e644c675272
sfjbssept <sfjbssept@gmail.com>
2022-10-13 15:34:24 +0000

    Added pensioner details service

---
+++ node-js/pension-management-system/isAuthenticated.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async function isAuthenticated(req, res, next) {
+  const token = req.headers["authorization"].split(" ")[1];
+
+  jwt.verify(token, "secret", (err, user) => {
+    if (err) {
+      return res.json({
+        sucess: 401,
+        message: "Unauthorized Access",
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
