480814bcfe90b3c6eef643a6247797abe57ce444
sfjbssept <sfjbssept@gmail.com>
2022-11-15 11:53:26 +0000

    Committing changes for state management

---
+++ angular-js/state-management-todo-list/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoReducer } from './reducers/todo.reducer';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { DisplayTodoComponent } from './components/display-todo/display-todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTodoComponent,
+    DisplayTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({todos: TodoReducer})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
